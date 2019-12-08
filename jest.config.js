0ed9c7ba77b92ff9562433ff7ba33097c22c9f75
thundermiracle <cissoliu@gmail.com>
2019-12-08 22:12:35 +0900

    add base settings for develop in typescript

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  roots: ['./src', './__test__'],
+
+  collectCoverage: true,
+
+  transformIgnorePatterns: ['/node_modules/(?!ramda).+\\.js$', 'dist'],
+  testPathIgnorePatterns: ['mocks'],
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  testRegex: '(/__test__/.*.(test|spec)).(jsx?|tsx?)$',
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  verbose: true,
+};
