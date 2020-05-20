2c87b040d01306750bb31cdba7b1fecc15b81bf0
thundermiracle <cissoliu@gmail.com>
2020-05-20 21:48:52 +0900

    resolve #4 use rollup to bundle umd package

---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
+
+export default [
+  // browser-friendly UMD build
+  {
+    input: 'src/index.ts',
+    output: {
+      name: 'FilterData',
+      file: 'dist/bundle.min.js',
+      format: 'umd',
+    },
+    plugins: [resolve(), commonjs(), typescript(), terser()],
+  },
+];
