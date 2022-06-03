b3982a5091cde8c172abaf18a2c0712c5b11e80a
ThunderMiracle <cissoliu@gmail.com>
2022-06-03 21:53:19 +0900

    feat: use @web-configs/eslint (#40)

--- src/filters/lessOrEqual.ts
+++ src/filters/lessOrEqual.ts
@@ -1,6 +1,7 @@
-import greater from './greater';
 import { complement } from '../lib/utils';
 import targetValueArray from '../prefilters/targetValueArray';
+
+import greater from './greater';
 
 /**
  * less or equal check for data
